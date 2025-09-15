e81c83a87b8f45f24bc76446e8773063f00ab3ae
Harold Carrillo <HAROLD.CARRILLO@ucr.ac.cr>
2025-09-15 12:02:17 -0600

    added cors and fixed lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
+import cors from "cors";
 import express from "express";
 
 import usersRoute from "./routes/cursos.js"
 
-const app = express();
 
+const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors({
+  origin: "http://localhost:8080"
+}));
 
 app.get("/health", (req, res) => {
   res.json({ok: true});
 });
 
 app.use(express.json());
-
 app.use('/',usersRoute);
 
 app.listen(port, () => {
