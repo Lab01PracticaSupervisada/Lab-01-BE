f28fa0c6dfdb31691d5130e1078f14caaa8eba84
Andres Diaz <andres.diaz2070@gmail.com>
2025-09-15 19:44:28 -0600

    fix: update directory config in jest and ts

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,7 @@ export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  roots: ['./'],
+  roots: ['./tests'],
   transform: {
     '^.+\\.ts?$': [
       'ts-jest',
